000b2ba41986b588d6f0d8ac9a1863c015101ad2
Hilary <hmlee2008@gmail.com>
2014-09-14 12:14:05 -0500

    Exercise from Pine book chapter 2

---
+++ pineex2.rb
@@ -0,0 +1,8 @@
+x = 24*365
+puts "There are #{x} hours in a year."
+y = ((((60*24)*7)*52)*10)
+puts "There are #{y} minutes in a decade."
+z = (((((60*60)*24)*365)*28) + (((60*60)*24)*131))
+puts "As of 9/13/2014, Hilary is #{z} seconds old."
+a = (1160000000 / (((60*60)*24)*365))
+puts "Pine is #{a} years old."
