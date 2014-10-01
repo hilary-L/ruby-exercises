6c290ed6cdbc96605f165184a42f25b3e056a4c9
Hilary <hmlee2008@gmail.com>
2014-10-01 13:44:52 -0500

    Pine Chapter 5 exercise 5.6

---
+++ pine5-6.rb
@@ -0,0 +1,7 @@
+puts 'Hello there. What\'s your first name?'
+firstname = gets.chomp
+puts 'And what is your middle name?'
+middlename = gets.chomp
+puts 'Finally, what is your last name?'
+lastname = gets.chomp
+puts 'Well, hi there ' + firstname + ' ' + middlename + ' ' + lastname + '!'
